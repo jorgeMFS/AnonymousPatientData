ee6eaf3d812d060e61f0b60f28bd40a5108331d8
jorgeMFS <jorge.miguel.ferreira.silva@gmail.com>
2016-12-06 20:04:32 +0000

    bug fix

--- src/test/java/pt/ieeta/anonymouspatientdata/core/impl/PersistantDataLuceneTest.java
+++ src/test/java/pt/ieeta/anonymouspatientdata/core/impl/PersistantDataLuceneTest.java
@@ -31,8 +31,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+
 
 /**
  * @author Jorge Miguel Ferreira da Silva
